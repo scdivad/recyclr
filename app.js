ee30649c8e72d42a416e517622920f28761d9645
abhinildutt <abhinil.dutt@gmail.com>
2023-02-24 14:32:28 -0600

    skeleton website

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-import express from {Express}
+const http = require('http');
 
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+app.set('view engine', 'html');
+
+app.get('/', (req, res, next) => {
+    res.sendFile('404.html', {root : __dirname});
+})
+
+app.listen(3000, () => {
+    console.log("Server active on port http://localhost:3000");
+});
