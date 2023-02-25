4fb8543a95edb9bc077c11bb706c88359014f7dc
abhinildutt <abhinil.dutt@gmail.com>
2023-02-25 17:14:52 -0600

    first merge

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ const router = express.Router();
 app.set('view engine', 'html');
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res, next) => {
+    res.sendFile('/public/html/index.html', {root : __dirname});
+})
+
 app.get('/error', (req, res, next) => {
     res.sendFile('404.html', {root : __dirname});
 });
 
-app.get('/', (req, res, next) => {
-    res.sendFile('/public/map.html', {root : __dirname});
+app.get('/manager_map', (req, res, next) => {
+    res.sendFile('/public/html/map.html', {root : __dirname});
+});
+
+
+app.get('/user_map', (req, res, next) => {
+    res.sendFile('/public/html/map.html', {root : __dirname});
 });
 
 app.listen(3000, () => {
---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+var getting_started_btn = document.getElementById("get_started");
+
+getting_started_btn.onclick() = function() {
+    window.location = '/user_map';
+}
