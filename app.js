d4e451375067fd673b72c5dbc12fc5c3ead11810
scdivad <davidsc2@illinois.edu>
2023-02-24 18:31:44 -0600

    works bitches

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const router = express.Router();
 
 app.set('view engine', 'html');
 
-app.get('/', (req, res, next) => {
+app.use(express.static(__dirname + '/public'));
+app.get('/error', (req, res, next) => {
     res.sendFile('404.html', {root : __dirname});
-})
+});
+
+app.get('/', (req, res, next) => {
+    res.sendFile('/public/map.html', {root : __dirname});
+});
 
 app.listen(3000, () => {
     console.log("Server active on port http://localhost:3000");
